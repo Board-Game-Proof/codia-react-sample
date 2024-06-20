d8de7b3dc1ba1069220f809d7bb394740d855d61
sangraal123 <googolheroicquests@gmail.com>
2024-06-20 18:16:04 +0900

    Aブロックのアップデート

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Page1 from './page1';
 import Page2 from './page2';
+import Page3 from './page3';
 
 export default function App() {
   return (
     <div>
       <Page1 />
       <Page2 />
+      <Page3 />
     </div>
   );
 }
---
+++ src/page3/index.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className='main-container'>
+      <div className='mask-group'>
+        <div className='arrow-left' />
+        <span className='board-game-sokratesra'>
+          ボードゲーム[ソクラテスラ] プレイ履歴証明
+        </span>
+        <div className='group'>
+          <div className='div' />
+        </div>
+        <div className='rectangle' />
+        <span className='span'>9:41</span>
+        <span className='span-1'>9:41</span>
+        <div className='div-2'>
+          <div className='battery' />
+        </div>
+        <div className='wi-fi' />
+        <div className='wi-fi-3' />
+        <div className='combined-shape' />
+        <div className='combined-shape-4' />
+      </div>
+      <span className='date-info'>
+        20XX年XX月XX日X曜日 <br />
+      </span>
+      <span className='player-info'>
+        ・プレイ人数：4人 <br />
+        ・TAROU, GENKI, JIROU, HANA <br />
+        ・プレイ回数累計：0回 <br />
+        ・拡張版：プレイ済
+      </span>
+      <div className='rectangle-5' />
+      <div className='rectangle-6'>
+        <span className='home-link'>ホームに戻る</span>
+      </div>
+      <div className='rectangle-7'>
+        <div className='copy-document'>
+          <div className='group-8' />
+        </div>
+        <div className='group-9' />
+      </div>
+      <span className='free-description'>
+        
+        自由記述欄
+        <br />
+        最後のターンでまさかの大逆転！ 手に汗握る攻防戦がとても楽しかったです！
+      </span>
+    </div>
+  );
+}
