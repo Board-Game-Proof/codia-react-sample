fc27990a0bb1b0aa4ba546a2827634d17876a445
sangraal123 <googolheroicquests@gmail.com>
2024-06-20 17:27:03 +0900

    lolol

---
+++ src/page1/index.jsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className='main-container'>
+      <div className='flex-row-ffd'>
+        <div className='frame' />
+        <div className='mask-group'>
+          <span className='tarou'>TAROU</span>
+          <div className='group'>
+            <div className='empty' />
+          </div>
+          <div className='rectangle' />
+          <span className='time'>9:41</span>
+          <div className='battery' />
+          <div className='wi-fi' />
+          <div className='combined-shape' />
+        </div>
+        <div className='rectangle-1'>
+          <div className='rectangle-2' />
+          <span className='play-history'>プレイ歴〇年〇ヵ月</span>
+        </div>
+      </div>
+      <div className='flex-row-af'>
+        <div className='ellipse' />
+        <div className='ellipse-3' />
+        <div className='ellipse-4' />
+      </div>
+      <div className='flex-row-d'>
+        <div className='ellipse-5' />
+        <div className='ellipse-6' />
+        <div className='ellipse-7' />
+      </div>
+      <div className='flex-row-c'>
+        <div className='ellipse-8' />
+        <div className='ellipse-9' />
+        <div className='ellipse-a' />
+      </div>
+      <div className='flex-row'>
+        <div className='ellipse-b' />
+        <div className='ellipse-c' />
+        <div className='ellipse-d' />
+      </div>
+      <div className='flex-row-cd'>
+        <div className='ellipse-e' />
+        <div className='ellipse-f' />
+        <div className='ellipse-10' />
+      </div>
+      <div className='flex-row-d-11'>
+        <div className='rectangle-12'>
+          <div className='flex-row-cf'>
+            <div className='rectangle-13'>
+              <div className='home'>
+                <div className='vector' />
+              </div>
+              <span className='home-14'>Home</span>
+            </div>
+            <div className='rectangle-15'>
+              <div className='rectangle-16'>
+                <div className='home-17'>
+                  <div className='vector-18' />
+                  <div className='home-19'>
+                    <div className='group-1a' />
+                  </div>
+                </div>
+                <div className='flex-row-ff'>
+                  <span className='home-1b'>Home</span>
+                  <span className='add'>Add</span>
+                </div>
+              </div>
+            </div>
+            <div className='frame-1c'>
+              <div className='group-1d'>
+                <span className='list'>List</span>
+                <div className='rectangle-1e' />
+              </div>
+              <div className='group-1f'>
+                <div className='flex-row-f'>
+                  <div className='vector-20' />
+                  <div className='vector-21' />
+                </div>
+                <div className='flex-row-eb'>
+                  <div className='vector-22' />
+                  <div className='vector-23' />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='home-indicator' />
+        </div>
+        <div className='ellipse-24' />
+        <div className='ellipse-25' />
+        <div className='ellipse-26' />
+      </div>
+      <div className='flex-row-ce'>
+        <div className='ellipse-27' />
+        <div className='ellipse-28' />
+        <div className='ellipse-29' />
+      </div>
+      <div className='flex-row-ef'>
+        <div className='ellipse-2a' />
+        <div className='ellipse-2b' />
+        <div className='ellipse-2c' />
+      </div>
+    </div>
+  );
+}
